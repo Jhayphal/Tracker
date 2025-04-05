ee5cbd46f272153e2fd733f43f09753f4b0c8553
oleg.nazarenko.dev <oleg.nazarenko.dev@gmail.com>
2025-04-05 18:50:45 +0300

    Set columns

--- Tracker/MainForm.Designer.cs
+++ Tracker/MainForm.Designer.cs
@@ -33,6 +33,10 @@
             this.btnEdit = new System.Windows.Forms.Button();
             this.btnRemove = new System.Windows.Forms.Button();
             this.dgvRecords = new System.Windows.Forms.DataGridView();
+            this.CreatedAt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Total = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Comment = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tlpContent.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvRecords)).BeginInit();
             this.SuspendLayout();
@@ -48,7 +52,7 @@
             this.tlpContent.Controls.Add(this.dgvRecords, 0, 0);
             this.tlpContent.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tlpContent.Location = new System.Drawing.Point(16, 16);
-            this.tlpContent.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.tlpContent.Margin = new System.Windows.Forms.Padding(6);
             this.tlpContent.Name = "tlpContent";
             this.tlpContent.RowCount = 3;
             this.tlpContent.RowStyles.Add(new System.Windows.Forms.RowStyle());
@@ -63,7 +67,7 @@
             // 
             this.btnAdd.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnAdd.Location = new System.Drawing.Point(2336, 6);
-            this.btnAdd.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(6);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(150, 45);
             this.btnAdd.TabIndex = 0;
@@ -73,7 +77,7 @@
             // btnEdit
             // 
             this.btnEdit.Location = new System.Drawing.Point(2336, 63);
-            this.btnEdit.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(6);
             this.btnEdit.Name = "btnEdit";
             this.btnEdit.Size = new System.Drawing.Size(150, 45);
             this.btnEdit.TabIndex = 1;
@@ -83,7 +87,7 @@
             // btnRemove
             // 
             this.btnRemove.Location = new System.Drawing.Point(2336, 120);
-            this.btnRemove.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(6);
             this.btnRemove.Name = "btnRemove";
             this.btnRemove.Size = new System.Drawing.Size(150, 45);
             this.btnRemove.TabIndex = 2;
@@ -96,9 +100,14 @@
             this.dgvRecords.AllowUserToDeleteRows = false;
             this.dgvRecords.AllowUserToOrderColumns = true;
             this.dgvRecords.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRecords.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CreatedAt,
+            this.Description,
+            this.Total,
+            this.Comment});
             this.dgvRecords.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvRecords.Location = new System.Drawing.Point(6, 6);
-            this.dgvRecords.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.dgvRecords.Margin = new System.Windows.Forms.Padding(6);
             this.dgvRecords.Name = "dgvRecords";
             this.dgvRecords.ReadOnly = true;
             this.dgvRecords.RowHeadersWidth = 92;
@@ -107,16 +116,56 @@
             this.dgvRecords.Size = new System.Drawing.Size(2318, 1260);
             this.dgvRecords.TabIndex = 3;
             // 
+            // CreatedAt
+            // 
+            this.CreatedAt.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.CreatedAt.DataPropertyName = "CreatedAt";
+            this.CreatedAt.HeaderText = "Created At";
+            this.CreatedAt.MinimumWidth = 12;
+            this.CreatedAt.Name = "CreatedAt";
+            this.CreatedAt.ReadOnly = true;
+            this.CreatedAt.Width = 203;
+            // 
+            // Description
+            // 
+            this.Description.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Description.DataPropertyName = "Description";
+            this.Description.HeaderText = "Description";
+            this.Description.MinimumWidth = 12;
+            this.Description.Name = "Description";
+            this.Description.ReadOnly = true;
+            this.Description.Width = 211;
+            // 
+            // Total
+            // 
+            this.Total.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Total.DataPropertyName = "Total";
+            this.Total.HeaderText = "Total";
+            this.Total.MinimumWidth = 12;
+            this.Total.Name = "Total";
+            this.Total.ReadOnly = true;
+            this.Total.Width = 132;
+            // 
+            // Comment
+            // 
+            this.Comment.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Comment.DataPropertyName = "Comment";
+            this.Comment.HeaderText = "Comment";
+            this.Comment.MinimumWidth = 12;
+            this.Comment.Name = "Comment";
+            this.Comment.ReadOnly = true;
+            this.Comment.Width = 190;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(2524, 1304);
             this.Controls.Add(this.tlpContent);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.MinimumSize = new System.Drawing.Size(1676, 848);
             this.Name = "MainForm";
-            this.Padding = new System.Windows.Forms.Padding(16, 16, 16, 16);
+            this.Padding = new System.Windows.Forms.Padding(16);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tracker";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
@@ -135,6 +184,10 @@
         private System.Windows.Forms.Button btnEdit;
         private System.Windows.Forms.Button btnRemove;
         private System.Windows.Forms.DataGridView dgvRecords;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CreatedAt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Description;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Total;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Comment;
     }
 }
 
